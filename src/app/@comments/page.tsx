d2f7aa5f28eb49721f110e7ece88f9fc93a741cf
Ilyes.dragovic <mingo.huncho@gmail.com>
2025-04-17 22:15:31 +0100

    init repo

---
+++ src/app/@comments/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Comments = () => {
+  return <div className="border p-[10rem] w-[30rem]">Comments</div>;
+};
+
+export default Comments;
---
+++ src/app/@members/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Members = () => {
+  return <div className="border p-[10rem] w-[30rem]">Members</div>;
+};
+
+export default Members;
